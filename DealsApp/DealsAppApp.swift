55e7b09fd2e314f1ee94ddd1ced3d30d014b9dd0
Syed Raza <syed.raza011@gmail.com>
2023-07-03 11:13:19 -0400

    Initial Commit

---
+++ DealsApp/DealsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DealsAppApp.swift
+//  DealsApp
+//
+//  Created by Syed Raza on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DealsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
